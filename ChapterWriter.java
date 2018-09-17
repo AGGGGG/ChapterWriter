a9b22cbdc4f1de5dbfb469bec25c24808625aa46
AGGGGG <andrewgaut.ag@gmail.com>
2018-09-17 16:57:31 -0700

    Update and rename GreatGatsby2.java to ChapterWriter.java

--- ChapterWriter.java
+++ ChapterWriter.java
@@ -2,21 +2,23 @@ import java.io.*;
 import java.util.*;
 
 public class GreatGatsby2 {
-	//COMMENTS: -- 
-	//run through Gatsby
-	//store 'order' letter words in hashtable as keys for Whatever values
+	//go through the whole input book
+	//store 'order' letter words in hashtable as keys for CharCombo values
 	//write file with numChars characters using hashtable and chooseNext() method
 	
-	private Hashtable <String, Whatever> hashtable = new Hashtable <String, Whatever> ();
+	private Hashtable <String, CharCombo> hashtable = new Hashtable <String, CharCombo> ();
 
 	//run through gatsby, add 'order' letter combos to hashtable, then write
-	public GreatGatsby2 (String readFileName, String writeFileName, int order, int numChars) throws IOException {
+	public ChapterWriter (String readFileName, String writeFileName, int order, int numChars) throws IOException {
 		FileReader fr = new FileReader (readFileName);
 		BufferedReader br = new BufferedReader (fr);
 
+		//iterate through whole book
 		while (br.ready()) {
 			String combo = "";
 			
+			//here, take in 'order' number of letters to 
+			//create a character string of length order
 			for (int i = 0; i <= order; i++) {
 				if (i==1) {
 					br.mark(order+2);
@@ -26,10 +28,12 @@ public class GreatGatsby2 {
 			if (hashtable.containsKey(combo)) {
 				hashtable.get(combo).incrementCountValue();
 			} else {
-				hashtable.put(combo, new Whatever());
+				hashtable.put(combo, new CharCombo());
 			}
-			hashtable.get(combo).getList().add((char) br.read());
-			br.reset();
+			//add next char to list of characters for CharCombo assocociated
+			//with this particular letter combo in hashtable
+			hashtable.get(combo).getList().add((char) br.read()); 
+			br.reset(); //reset bufferedreader so we don't miss character combos
 		}
 		fr.close();
 		br.close();
@@ -37,6 +41,7 @@ public class GreatGatsby2 {
 		File writeFile = new File (writeFileName);
 		PrintWriter pw = new PrintWriter(writeFile);
 		
+		//now, write the chapter
 		//print out first combo (random key from hashtable), then choose next until you reach numChars
 		String combo = hashtable.keys().nextElement();  //not sure if this is super inefficient, but it only gets called once
 		pw.print(combo); 
