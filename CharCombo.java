adad5699419e036f8612c49533142f90707f54b8
AGGGGG <andrewgaut.ag@gmail.com>
2018-09-17 16:54:03 -0700

    Update CharCombo.java

--- CharCombo.java
+++ CharCombo.java
@@ -3,6 +3,8 @@ import java.util.*;
 public class CharCombo {
 	//the amount of times letter combo appears
 	private int countValue;
+	
+	//list of characters that come after this particular character combo
 	private ArrayList <Character> list;
 	
 	public CharCombo() {
@@ -23,6 +25,9 @@ public class CharCombo {
 	}
 	
 	//get next letter by randomly searching through list
+	//list will contain duplicates of letters each time they show up,
+	//so that we need only choose a letter randomly and 
+	//their frequency will automatically be weighted.
 	public Character chooseNext() {
 		return list.get((int) (Math.random() * list.size()));
 	}
